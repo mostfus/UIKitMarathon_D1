8af4869e204c07239245821bd71bffed3a0e7fda
mostfus <mostfus@yandex-team.ru>
2024-02-03 22:19:38 +0300

    [refactor] make view as container for gradient

--- UIKitMarathon_D1/ViewController.swift
+++ UIKitMarathon_D1/ViewController.swift
@@ -9,53 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    enum Const {
-        static let hundredPt: CGFloat = 100.0
-        static let cornerRadius = 20.0
-    }
-
-    private var gradientLayer: CAGradientLayer = {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [UIColor.systemPink.cgColor, UIColor.black.cgColor]
-        gradientLayer.startPoint = .init(x: 0, y: 0)
-        gradientLayer.endPoint = .init(x: 1, y: 1)
-        gradientLayer.cornerRadius = Const.cornerRadius
-        return gradientLayer
-    }()
-
-    private var gradientView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-
-        view.layer.shadowColor = UIColor.black.cgColor
-        view.layer.shadowOffset = .init(width: 10, height: 10)
-        view.layer.shadowRadius = 5
-        view.layer.shadowOpacity = 0.3
-
-        return view
-    }()
+    private lazy var gradientView = GradientView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        gradientView.layer.addSublayer(gradientLayer)
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        gradientView.layer.cornerRadius = 20.0
+        gradientView.layer.cornerCurve = .continuous
+
         view.addSubview(gradientView)
 
         setupConstrains()
     }
 
-    override func viewDidLayoutSubviews() {
-        gradientLayer.frame = gradientView.bounds
-    }
-
-
     private func setupConstrains() {
         NSLayoutConstraint.activate([
-            gradientView.widthAnchor.constraint(equalToConstant: Const.hundredPt),
-            gradientView.heightAnchor.constraint(equalToConstant: Const.hundredPt),
-            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Const.hundredPt),
+            gradientView.widthAnchor.constraint(equalToConstant: 100),
+            gradientView.heightAnchor.constraint(equalToConstant: 100),
+            gradientView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
             gradientView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
+    }
+}
+
+class GradientView: UIView {
+    private let gradientLayer = CAGradientLayer()
+
+    init() {
+        super.init(frame: .zero)
+
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .init(width: 10, height: 10)
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.4
+
+        gradientLayer.colors = [UIColor.systemPink.cgColor, UIColor.black.cgColor]
+        gradientLayer.startPoint = .init(x: 0, y: 0)
+        gradientLayer.endPoint = .init(x: 1, y: 1)
+
+        layer.addSublayer(gradientLayer)
+    }
+
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+
+        if layer == self.layer {
+            gradientLayer.frame = bounds
+            gradientLayer.cornerRadius = layer.cornerRadius
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
